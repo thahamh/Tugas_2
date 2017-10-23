016593561c74c6706fa5ea9ca37d6c9621f5bd4e
thahamh <32389972+thahamh@users.noreply.github.com>
2017-10-22 22:44:49 -0700

    Update Tugas4.cpp

--- Tugas4.cpp
+++ Tugas4.cpp
@@ -5,7 +5,7 @@ int main (){
 	
 	double luasRumah = 42;
 	double lantaiP = 193*1215;
-	double luasBox = lantaiP*8/1000000;
+	double luas1Box = lantaiP*8/1000000;
 	cout << "Luas Rumah	: "<<luasRumah<<" m2"<<endl;
 	cout << "Luas 1 box parket : "<<luas1Box<<" m2"<<endl;
 }
